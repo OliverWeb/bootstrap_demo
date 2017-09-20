2a5a6ddd3a07c182cfb5ac97884e1bc8cc11c801
OliverWeb <Oliver_web@163.com>
2017-09-20 19:52:24 +0800

    first commit

--- Oliver_demo/js/table.js
+++ Oliver_demo/js/table.js
@@ -1,5 +1,17 @@
 //进行表格添加//进行表格添加
 $("#add_pool").click(function() {
+  if($(".pool_edit").length>0){
+    var did_creat=$(".pool_edit");
+    console.log(did_creat.length);
+    var did_creat_key= $(did_creat[did_creat.length-1]).attr("key");
+  }else{
+    did_creat_key=-1
+  }
+
+  // for(var i=0;i<did_creat.length;i++){
+  //   var did_creat_key=$(did_creat[i].find("span").attr("key"));
+  // }
+
   var add_pool = `<tr class="">
                     <td class="" width="45%">
                         <div class="" style="width:30%;display:inline-block;">
@@ -9,13 +21,13 @@ $("#add_pool").click(function() {
                           <span class="label label-danger">Delete</span>
                         </a>
                     </td>
-                    <td><a class="pool_edit" href="javascript:;"><span class="label label-success">Edit</span></a></td>
+                    <td><a class="pool_edit" key=${did_creat_key+1} href="javascript:;"><span class="label label-success">Edit</span></a></td>
                     <td><a class="pool_view" href="javascript:;"><span class="label label-primary">Detail</span></a></td>
                   </tr>`;
   $('#pool_name_set').append(add_pool);
 });
 //表格删除
-$(".pool_delete").click(function() {
+$("body").on("click",".pool_delete",function() {
   var _this = this;
   bootbox.confirm({
     message: "是否确认删除池?如果是请点击Yes否则点击No!",
@@ -33,6 +45,7 @@ $(".pool_delete").click(function() {
       console.log('This was logged in the callback: ' + result);
       if (result) {
         console.log($(_this).parent().parent().remove());
+        setTimeout(function(){location.reload()},100);
       } else {
         console.log("取消");
       }
@@ -40,12 +53,84 @@ $(".pool_delete").click(function() {
   });
 
 });
-//模态框- 分片池设置
+
+// 请求已加入的数据
+
+// function did_data(){
+//   var _data="no have data";
+//   $.ajax({
+//     url: "./json/pool.json",
+//     contentType: "application/json",
+//     type: "get",
+//     // data:JSON.stringify({"name":name,"pwd":pwd}),
+//     dataType: "json",
+//     success:function(data){
+//       _data=data;
+//     },error:function(){
+//       console.log(服务器异常);
+//     }
+//   });
+//   return _data
+// }
+// did_data();
+//模态框- 分片池设置, 进行选择已加入服务和未加入服务
 $('body').on("click", ".pool_edit", function() {
+  click_index=$(this).attr("key");
   $('#setModal').modal('show');
-  var key=$(this).attr("key");
-  console.log(_data[key].key);
-  console.log(key);
+  title_value=$(this).parent().prev().find("input").val();   //用于提交数据用
+  if($(this).attr("key")){
+    var index=$(this).attr("key");
+  }else{
+    var index=-1;   //判断是页面的初始数据还是新增的数据
+  }
+  $.ajax({     //返回所有池的名称
+    url: "./json/pool_all.json",
+    contentType: "application/json",
+    type: "get",
+    // data:JSON.stringify({"name":name,"pwd":pwd}),
+    dataType: "json",
+    success:function(data){
+    if(index!=-1){     //页面初始数据
+      var data_did=_data[index].servers;
+      console.log("已经加入的数据的个数:"+data_did.length);    //已加入数据的个数
+      console.log("这里数据总的个数:"+data.servers.length); //总的数据个数
+      var data_no=data.servers.filter(
+      function(e){ return data_did.indexOf(e) < 0;
+      });
+    }else{data_did=[];data_no=data.servers;}
+    console.log("未加入数据的个数"+data_no.length);  //未加入的数据的个数
+    //将未加入的数据进行遍历
+    data_no.map(function(value,key){
+        $('.multi-select').append($("<option value='" +
+          value +
+          "'"  +
+          "selected"+
+          ">" +
+           value+
+          "</option>"));
+    });
+    $('.multi-select').multiSelect('refresh'); //刷新多选下拉标签
+    },
+    error:function(){
+      alert("服务器异常");
+    }
+  });
+  if(index!=-1){
+    $('.pool_name_title').html(_data[index].key);
+    $('.multi-select').empty();//清空下拉标签
+    _data[index].servers.map(function(value,key){
+        $('.multi-select').append($("<option value='" +
+          value +
+          "'"  +
+          ">" +
+           value+
+          "</option>"));
+    });
+  }else{
+    $('.multi-select').empty();//清空下拉标签
+  }
+  $('.multi-select').multiSelect('refresh'); //刷新多选下拉标签
+
 });
 // 产看信息模态框
 $('body').on("click", ".pool_view", function() {
@@ -82,9 +167,25 @@ function viewsearch() {
 
 //异步进行提交分片池配置
 function fenpianchi_submit() {
-  var poolName = $('#poolName').val();
-  var datas = "poolname=" + poolName + "&" + $.ajax({
-    type: "POST", dataType: "json", //服务端接收的数据类型
+ console.log("key:"+click_index);
+  var servers = [];//提交数组
+  var did_join_html=$(".ms-elem-selectable:visible").find('span');
+  console.log(did_join_html.length);
+  for(var i=0;i<(did_join_html.length);i++){
+    console.log($(did_join_html[i]).html());
+    servers.push($(did_join_html[i]).html());
+  }
+  //数据进行去重
+    var did_arr=_data[click_index].servers;
+    servers=data.servers.filter(
+    function(e){ return did_arr.indexOf(e) < 0;
+    });
+  var poolName = title_value;
+  console.log(poolName);
+  var datas = "poolname=" + poolName+"&"+"servers="+servers;
+  $.ajax({
+    type: "POST",
+    dataType: "json", //服务端接收的数据类型
     url: "/users/login",
     data: datas,
     success: function(result) {
@@ -94,19 +195,22 @@ function fenpianchi_submit() {
       };
     },
     error: function() {
-      alert("异常！");
+       console.log("提交异常");
     }
   });
 };
 $('body').on("click", ".pool_submit_btn", function() {
   fenpianchi_submit();
+  location.reload();
 });
 //分片池首页的请求加载  start
 
 function pool_name_list() {
   $('.pool_set_box').empty();
   $.ajax({
-    url: "./json/pool.json", contentType: "application/json", type: "get",
+    url: "./json/pool.json",
+    contentType: "application/json",
+    type: "get",
     // data:JSON.stringify({"name":name,"pwd":pwd}),
     dataType: "json",
     success: function(data) {
@@ -126,7 +230,6 @@ function pool_name_list() {
               <td><a class="pool_edit" key=${key} href="javascript:;"><span class="label label-success">Edit</span></a></td>
               <td><a class="pool_view" href="javascript:;"><span class="label label-primary">Detail</span></a></td>
             </tr>`;
-        console.log(value.key);
         $('.pool_set_box').append(pool_name_html);
       });
     },
@@ -178,11 +281,3 @@ $(function() {
 // 34         }
 //
 // $(".ms-elem-selection:hidden")  在未选中列表中的选着隐藏的元素进行提交;
-$('.multi-select').append($("<option value='" +
-  "AA" +
-  "'" +
-  "selected" +
-  ">" +
-  "AAAAAAAAA" +
-  "</option>"));
-$('.multi-select').multiSelect('refresh'); //刷新多选下拉标签
